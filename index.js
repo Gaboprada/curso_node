0e1a8470b93050eb8aedc0b326a022127cc66b6a
gaboprada <gaboprada@gmai.com>
2025-04-02 23:19:25 -0500

    exportarfunciones en js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-console.log ('hola mundo');
-console.info('hola mundo');
-console.error('hola mundo');
+import {sum,mul,div, rest} from "./sum.js";
+
+
+console.log(sum(1,2));
+console.log(rest(1,2));
+console.log(mul(1,2));
+console.log(div(1,2));
